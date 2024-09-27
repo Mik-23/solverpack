3c54b479bf862b8f147808f655c120fa6d0de06e
Mik-23 <159887945+Mik-23@users.noreply.github.com>
2024-09-27 13:12:37 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+  name='solverpack',
+  version='0.0.1',
+  author='mikhail_rozhkov',
+  description='Package solver.',
+  url='your_url',
+  packages=find_packages(),
+  install_requires=['pandas==2.2.2',
+                    'sqlalchemy==2.0.35',
+                    'psycopg2==2.9.9'],
+)
