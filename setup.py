615632f023bd39f742841bdbe5649730c62e609d
Mik-23 <159887945+Mik-23@users.noreply.github.com>
2024-09-29 16:43:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
   version='0.0.1',
   author='mikhail_rozhkov',
   description='Package solver.',
-  url='your_url',
   packages=find_packages(),
   install_requires=['pandas==2.2.2',
                     'sqlalchemy==2.0.35',
